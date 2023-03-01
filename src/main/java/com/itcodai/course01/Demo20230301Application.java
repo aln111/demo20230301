3eefebd243f18c577d0fcd22e43e49801b53dd0d
Administrator <123178062@qq.ocm>
2023-03-01 09:03:34 +0800

    Initial commit

---
+++ src/main/java/com/itcodai/course01/Demo20230301Application.java
@@ -0,0 +1,13 @@
+package com.itcodai.course01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo20230301Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo20230301Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/itcodai/course01/Demo20230301ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itcodai.course01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo20230301ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
